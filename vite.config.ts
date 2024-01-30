2f80b28dff646c76393346a4904c1dc7372abe77
Bruce <recall4056@gmail.com>
2024-01-30 17:31:47 +0800

    chore: update env

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,67 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { BootstrapAnimation } from '@raipiot-infra/bootstrap-animation'
 import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import Icons from 'unplugin-icons/vite'
+import type { ProxyOptions } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig(({ mode }) => ({
-  plugins: [react(), BootstrapAnimation()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  const {
+    VITE_PORT,
+    VITE_BASE_API_PREFIX,
+    VITE_BASE_API_URL,
+    VITE_MOCK_API_PREFIX,
+    VITE_MOCK_API_URL
+  } = env as ImportMetaEnv
+
+  const port = parseInt(VITE_PORT, 10) || 5173
+  const proxy: Record<string, string | ProxyOptions> = {
+    [VITE_BASE_API_PREFIX]: {
+      target: VITE_BASE_API_URL,
+      changeOrigin: true,
+      rewrite: (path: string) => path.replace(VITE_BASE_API_PREFIX, '')
     },
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
-  },
-  esbuild: {
-    drop: mode === 'production' ? ['console', 'debugger'] : []
+    [VITE_MOCK_API_PREFIX]: {
+      target: VITE_MOCK_API_URL,
+      changeOrigin: true,
+      rewrite: (path: string) => path.replace(VITE_MOCK_API_PREFIX, '')
+    }
   }
-}))
+
+  return {
+    base: '/',
+    plugins: [
+      react(),
+      BootstrapAnimation(),
+      Icons({
+        autoInstall: true,
+        compiler: 'jsx',
+        jsx: 'react'
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+    },
+    esbuild: {
+      drop: mode === 'production' ? ['console', 'debugger'] : []
+    },
+    server: {
+      host: true,
+      port,
+      strictPort: true,
+      open: false,
+      proxy
+    },
+    preview: {
+      host: true,
+      port,
+      strictPort: true,
+      open: false,
+      proxy
+    }
+  }
+})
