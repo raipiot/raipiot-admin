647bc6cc1ed93af3d065f694cbd5e674840ea512
Bruce <recall4056@gmail.com>
2024-01-30 11:38:30 +0800

    feat: :sparkles: init project

---
+++ README.md
@@ -0,0 +1,56 @@
+# raipiot-admin
+
+raipiot Admin 基于 React + TypeScript + Vite + antd + TailwindCSS。
+
+## 技术栈
+
+### 核心
+
+- React v18
+- TypeScript v5
+- Vite v5
+
+### 路由层
+
+- react-router v6
+
+### 状态管理层
+
+- Zustand v4
+- tanstack-query v5
+
+### 接口层
+
+- Axios
+
+### UI 层
+
+- antd v5
+- TailwindCSS
+- Sass
+- clsx
+
+### 工具类
+
+- dayjs
+- lodash-es
+- use-immer
+
+### 国际化
+
+- react-i18next
+
+### Vite 插件
+
+- unplugin-auto-import
+- unplugin-icons
+
+### 前端工程化
+
+- ESLint
+- Prettier
+- CSpell
+- Commitlint
+- husky
+- lint-staged
+- cz-git
---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+
+import viteLogo from '/vite.svg'
+
+import raipiotLogo from './assets/img/raipiot.png'
+import reactLogo from './assets/react.svg'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="m-auto flex h-screen max-w-[1280px] flex-col items-center justify-center">
+      <a
+        href="https://www.raipiot.com"
+        target="_blank"
+        rel="noreferrer"
+        className="mb-8"
+      >
+        <img
+          src={raipiotLogo}
+          className="w-32 hover:animate-pulse"
+          alt="raipiot logo"
+          loading="eager"
+        />
+      </a>
+      <div className="mb-8 flex items-center space-x-6">
+        <a
+          href="https://vitejs.dev"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={viteLogo}
+            className="w-20 hover:animate-bounce"
+            alt="Vite logo"
+            loading="eager"
+          />
+        </a>
+        <a
+          href="https://react.dev"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={reactLogo}
+            className="w-20 hover:animate-spin"
+            alt="React logo"
+            loading="eager"
+          />
+        </a>
+      </div>
+
+      <div className="mb-4 text-4xl">raipiot React Starter</div>
+      <div className="mb-4 text-xl">Vite + React + TypeScript</div>
+      <button
+        type="button"
+        onClick={() => setCount((v) => v + 1)}
+        className="mb-4 rounded-md bg-gray-200 p-2"
+      >
+        count is {count}
+      </button>
+      <div>Powered by raipiot Front-end Core Team</div>
+    </div>
+  )
+}
